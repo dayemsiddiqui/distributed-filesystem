70cacbe8a5de07204627e8134ec8d72e9e62ea88
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2017-04-21 20:54:24 +0500

    Client connect

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
 
-var prompt = require('prompt'); 
+var io = require('socket.io-client');
+
+var socket = io('http://192.168.15.53:3000/');
+
+var prompt = require('prompt');
 
 http.listen(3000, function(){
   console.log('Sever started on port 3000');
@@ -25,9 +29,9 @@ io.on('connection', function(socket){
 const main = () => {
     prompt.start();
      prompt.get(['username', 'email'], function (err, result) {
-    // 
-    // Log the results. 
-    // 
+    //
+    // Log the results.
+    //
     console.log('Command-line input received:');
     console.log('  username: ' + result.username);
     console.log('  email: ' + result.email);
