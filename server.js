12520be230c2c2f2311411b307db41aba7165771
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2017-04-25 18:38:09 +0500

    Figlet Title Added

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 import {getAnotherCommand} from './parser';
 import {initServer} from './connection';
 import {Actions} from './actions';
-
+var figlet = require('figlet');
 
 const main = () => {
 
+  figlet('Gormint FS', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+  });
   initServer();
   Actions.initializeFileTable();
   getAnotherCommand();
