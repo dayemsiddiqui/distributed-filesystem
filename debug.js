1bd7d93bb618b06a7fbca6a6917352571c6efcef
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2017-04-25 17:57:56 +0500

    Debug Transferred to a separate terminal

--- debug.js
+++ debug.js
@@ -1,3 +1,15 @@
+var Console = require('console').Console;
+var fs = require('fs')
+var stream = fs.createWriteStream('tmp/mylog', {
+  flags: 'w',
+  defaultEncoding: 'utf8',
+  fd: null,
+  mode: 0o666,
+  autoClose: true
+});
+var console = new Console(stream, stream);
+
+
 var debug = {}
 debug.log = (message, DEV) => {
   if(DEV){
