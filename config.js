604159e1e0d84fef2e21c2a1bb4bb4ced9962006
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2017-04-30 00:27:22 +0500

    Setup On Windows Machine
    Windows Machine Setup At Home

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 export const config = {
-  server_addr: ['192.168.15.53', '192.168.15.134'],
-  my_addr: '192.168.15.134',
+  server_addr: ['192.168.15.134'],
+  my_addr: '192.168.15.53',
   prompt: {
-            description: 'Type your command',     // Prompt displayed to the user. If not supplied name will be used.
+            description: ' ',     // Prompt displayed to the user. If not supplied name will be used.
             type: 'string',                 // Specify the type of input to expect.
             hidden: true,                        // If true, characters entered will either not be output to console or will be outputed using the `replace` string.
             replace: '',                        // If `hidden` is set it will replace each hi
